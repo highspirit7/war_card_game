bd32bbb4f45b10063e4d8a161168ee55d3b0b4bb
GrownUprince <jake.lee4006@gmail.com>
2023-12-06 15:31:34 +0100

    create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import random
+from card import Card, compare_two_cards, ascii_of_two_cards, ascii_of_cards_for_war
+
+ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+suits = ["♥️", "♦️", "♣️", "♠️"]
+
+
+def main():
+    user_card = Card("10", "♠️")
+    computer_card = Card("10", "♣️")
+    user_war_cards = []
+    computer_war_cards = []
+
+    for _ in range(4):
+        user_war_cards.append(Card(random.choice(ranks), random.choice(suits)))
+
+    for _ in range(4):
+        computer_war_cards.append(Card(random.choice(ranks), random.choice(suits)))
+
+    ascii_of_two_cards(user_card, computer_card)
+    ascii_of_cards_for_war(user_war_cards, computer_war_cards)
+
+
+if __name__ == "__main__":
+    main()
