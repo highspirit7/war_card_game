e5a2450c35d7a8353f9e2c0d93dfbdb05b10bd3d
GrownUprince <jake.lee4006@gmail.com>
2023-12-08 22:48:53 +0100

    Refactor : refactor WarGame class

--- war_game.py
+++ war_game.py
@@ -6,12 +6,9 @@ from utils import convert_to_ordinal
 
 
 class WarGame:
-    def __init__(self, user: Player, computer: Player, deck: Deck) -> None:
+    def __init__(self) -> None:
         self.round = 0
         self.is_game_over = False
-        self.user = user
-        self.computer = computer
-        self.deck = deck
 
     @property
     def round(self):
@@ -19,7 +16,7 @@ class WarGame:
 
     @round.setter
     def round(self, number):
-        if type(number) != "int":
+        if type(number) != int:
             raise TypeError("WarGame round type must be integer")
 
         if number < 0:
@@ -33,29 +30,29 @@ class WarGame:
 
     @is_game_over.setter
     def is_game_over(self, value):
-        if type(value) != "bool":
+        if type(value) != bool:
             raise TypeError("WarGame is_game_over must be boolean")
 
         self._is_game_over = value
 
     def ask_user_to_play(self, round: int) -> None:
-        user_input
+        user_input = ""
+
         if round:
             user_input = input(
                 "Do you want to continue playing into the next round?(yes/no): "
             )
         else:
             user_input = input("Do you want to start war game?(yes/no): ")
+        print()
         if user_input.lower()[0] == "n":
+            self.is_game_over = True
             self.exit_war_game()
 
-    def announce_winner_of_round(
-        player: Literal["user", "computer"], round: int
-    ) -> None:
+    def announce_winner_of_round(self, player: Literal["user", "computer"]) -> None:
         winner = "You" if player == "user" else "Computer"
         print(
-            "\n"
-            + f"{winner} has won in the {convert_to_ordinal(round)} round. So {winner} takes all the card on the table."
+            f"{winner} has won in the {convert_to_ordinal(self.round)} round. So {winner} takes all the card on the table."
             + "\n"
         )
 
@@ -68,12 +65,6 @@ class WarGame:
             + f"{round} rounds were held in total. and {winner} has won this war game!!"
         )
 
-    def show_cards_of_players(user: Player, computer: Player) -> None:
-        print(
-            f"====== Currently {user.type} has {len(user.card_pile)} card // {computer.type} has {len(computer.card_pile)} ======"
-            + "\n"
-        )
-
     def get_winner_of_game(
         user_cards, computer_cards
     ) -> Union[Literal["user", "computer"], None]:
@@ -84,7 +75,7 @@ class WarGame:
         else:
             return
 
-    def exit_war_game() -> None:
+    def exit_war_game(self) -> None:
         sys.exit(
             """
   _____                   ____                 
@@ -95,18 +86,16 @@ class WarGame:
 """
         )
 
-    def print_guide_before_start() -> None:
+    def print_guide_before_start(self) -> None:
         print(
             """
-    [PLAY GUIDE]
-    War(card game) is a fun card game where players try to win all of the cards in the deck.
-    This game uses one standard deck of playing cards.(the version of this war game does not use joker cards, 
-    so the total number of cards for this game is 52.)
-    Once you play this game, you will play against computer.
-    Your card will be located on the left side and
-    computer's card will be located on the right side on the table.
+[PLAY GUIDE]
+War(card game) is a fun card game where players try to win all of the cards in the deck.
+This game uses one standard deck of playing cards.(the version of this war game does not use joker cards, 
+so the total number of cards for this game is 52.)
+Once you play this game, you will play against computer.
+Your card will be located on the left side and
+computer's card will be located on the right side on the table.
 
     """
         )
-
-    
