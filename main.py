945fcb72a4fd6a0e072e82723686583e183039d2
GrownUprince <jake.lee4006@gmail.com>
2023-12-08 13:57:11 +0100

    create and complete WarGame class

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import random
 from card import Card, compare_two_cards, ascii_of_two_cards, ascii_of_cards_for_war
+from war_game import WarGame
+from player import Player
+from deck import Deck
 
 ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 suits = ["♥️", "♦️", "♣️", "♠️"]
@@ -17,8 +20,8 @@ def main():
     for _ in range(4):
         computer_war_cards.append(Card(random.choice(ranks), random.choice(suits)))
 
-    ascii_of_two_cards(user_card, computer_card)
-    ascii_of_cards_for_war(user_war_cards, computer_war_cards)
+    # ascii_of_two_cards(user_card, computer_card)
+    # ascii_of_cards_for_war(user_war_cards, computer_war_cards)
 
 
 if __name__ == "__main__":
---
+++ war_game.py
@@ -0,0 +1,110 @@
+from typing import Literal, Union
+import sys
+from player import Player
+from deck import Deck
+from utils import convert_to_ordinal
+
+
+class WarGame:
+    def __init__(self, user: Player, computer: Player, deck: Deck) -> None:
+        self.round = 0
+        self.is_game_over = False
+        self.user = user
+        self.computer = computer
+        self.deck = deck
+
+    @property
+    def round(self):
+        return self._round
+
+    @round.setter
+    def round(self, number):
+        if type(number) != "int":
+            raise TypeError("WarGame round type must be integer")
+
+        if number < 0:
+            raise ValueError("WarGame round value must be positive integer or 0")
+
+        self._round = number
+
+    @property
+    def is_game_over(self):
+        return self._is_game_over
+
+    @is_game_over.setter
+    def is_game_over(self, value):
+        if type(value) != "bool":
+            raise TypeError("WarGame is_game_over must be boolean")
+
+        self._is_game_over = value
+
+    def ask_user_to_play(self, round: int) -> None:
+        user_input
+        if round:
+            user_input = input(
+                "Do you want to continue playing into the next round?(yes/no): "
+            )
+        else:
+            user_input = input("Do you want to start war game?(yes/no): ")
+
+        return user_input.lower()[0]
+
+    def announce_winner_of_round(
+        player: Literal["user", "computer"], round: int
+    ) -> None:
+        winner = "You" if player == "user" else "Computer"
+        print(
+            "\n"
+            + f"{winner} has won in the {convert_to_ordinal(round)} round. So {winner} takes all the card on the table."
+            + "\n"
+        )
+
+    def announce_winner_of_game(
+        player: Literal["user", "computer"], round: int
+    ) -> None:
+        winner = "You" if player == "user" else "Computer"
+        print(
+            "\n"
+            + f"{round} rounds were held in total. and {winner} has won this war game!!"
+        )
+
+    def show_cards_of_players(user: Player, computer: Player) -> None:
+        print(
+            f"====== Currently {user.type} has {len(user.card_pile)} card // {computer.type} has {len(computer.card_pile)} ======"
+            + "\n"
+        )
+
+    def get_winner_of_game(
+        user_cards, computer_cards
+    ) -> Union[Literal["user", "computer"], None]:
+        if not len(user_cards):
+            return "computer"
+        elif not len(computer_cards):
+            return "user"
+        else:
+            return
+
+    def exit_war_game() -> None:
+        sys.exit(
+            """
+  _____                   ____                 
+ / ___/___ _ __ _  ___   / __ \ _  __ ___  ____
+/ (_ // _ `//  ' \/ -_) / /_/ /| |/ // -_)/ __/
+\___/ \_,_//_/_/_/\__/  \____/ |___/ \__//_/   
+                                               
+"""
+        )
+
+    def print_guide_before_start() -> None:
+        print(
+            """
+    [PLAY GUIDE]
+    War(card game) is a fun card game where players try to win all of the cards in the deck.
+    This game uses one standard deck of playing cards.(the version of this war game does not use joker cards, 
+    so the total number of cards for this game is 52.)
+    Once you play this game, you will play against computer.
+    Your card will be located on the left side and
+    computer's card will be located on the right side on the table.
+
+    """
+        )
