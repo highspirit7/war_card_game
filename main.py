fd46de335f9781b17be7de3c9357768ece575901
GrownUprince <jake.lee4006@gmail.com>
2023-12-09 16:45:10 +0100

    Feat & Refactor : finalize this program & refactoring

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from table import Table
 from card import (
-    Card,
     compare_two_cards,
     ascii_of_two_cards,
     ascii_of_cards_for_war,
-    print_cards,
 )
-from war_game import WarGame, get_winner_of_game
+from war_game import WarGame, get_winner_of_game, check_cards_before_war
 from player import Player, show_cards_of_players
 from deck import Deck
 
@@ -32,65 +30,56 @@ def main():
         game_table = Table([user_top_card], [computer_top_card])
         game_table.top_table_of_battle(game.round)
         ascii_of_two_cards(user_top_card, computer_top_card)
-        game_table.bottom_of_table()
+        game_table.bottom_of_table("battle")
 
-        # battle_result = compare_two_cards(user_top_card, computer_top_card)
-        battle_result = "tie"
+        battle_result = compare_two_cards(user_top_card, computer_top_card)
+        war_result = None
 
         if battle_result == "tie":
             # before going to war, need to check if all players have enough cards for war
             while not game_table.is_war_over:
-                result = get_winner_of_game(user.card_pile, computer.card_pile)
-                game.announce_war(result)
+                result = check_cards_before_war(user.card_pile, computer.card_pile)
 
-                if result != None:
-                    game.announce_winner_of_game(result, game.round)
+                if result != "continue":
+                    game.announce_winner_of_game(result)
                     game.exit_war_game()
 
-                # print_cards(user.card_pile, "user")
-                # print_cards(computer.card_pile, "computer")
+                war_result = go_to_war(game, game_table, user, computer)
 
-                game_table.war_count += 1
-                game_table.top_table_of_war(game.round)
-
-                this_war_user_cards = user.flip_cards_for_war()
-                this_war_computer_cards = computer.flip_cards_for_war()
-                game_table.cards.extend(this_war_user_cards + this_war_computer_cards)
-                game_table.user_cards.extend(this_war_user_cards)
-                game_table.computer_cards.extend(this_war_computer_cards)
-
-                # print_cards(game_table.cards, "table")
-                # print_cards(game_table.user_cards, "user_table")
-                # print_cards(game_table.computer_cards, "computer_table")
-
-                ascii_of_cards_for_war(this_war_user_cards, this_war_computer_cards)
-                game_table.bottom_of_table()
-
-                war_result = compare_two_cards(
-                    game_table.user_cards[-1], game_table.computer_cards[-1]
-                )
                 if war_result != "tie":
                     game_table.is_war_over = True
 
-            winner = user if war_result == "user" else computer
-            winner.win_cards(game_table.cards)
-            game.announce_winner_of_round(war_result)
-
-            # print_cards(user.card_pile, "user")
-            # print_cards(computer.card_pile, "computer")
-        else:
-            winner = user if battle_result == "user" else computer
-            winner.win_cards(game_table.cards)
-            game.announce_winner_of_round(battle_result)
+        result_of_round = battle_result if war_result == None else war_result
+        winner = user if result_of_round == "user" else computer
+        winner.win_cards(game_table.cards)
+        game.announce_winner_of_round(result_of_round)
 
         show_cards_of_players(user, computer)
+
+        # check each player's deck in order to find if we have a winner in this whole game after each round
+        winner = get_winner_of_game(user.card_pile, computer.card_pile)
+        if winner != None:
+            game.announce_winner_of_game(winner)
+            game.exit_war_game()
+
         game.ask_user_to_play(game.round)
 
-        # TODO : Need to check each player's deck in order to find if we have a winner in this whole game
 
+def go_to_war(game: WarGame, game_table: Table, user: Player, computer: Player):
+    game.announce_war()
+    game_table.war_count += 1
+    game_table.top_table_of_war(game.round)
 
-def go_to_war():
-    pass
+    this_war_user_cards = user.flip_cards_for_war()
+    this_war_computer_cards = computer.flip_cards_for_war()
+    game_table.cards.extend(this_war_user_cards + this_war_computer_cards)
+    game_table.user_cards.extend(this_war_user_cards)
+    game_table.computer_cards.extend(this_war_computer_cards)
+
+    ascii_of_cards_for_war(this_war_user_cards, this_war_computer_cards)
+    game_table.bottom_of_table("war")
+
+    return compare_two_cards(game_table.user_cards[-1], game_table.computer_cards[-1])
 
 
 if __name__ == "__main__":
